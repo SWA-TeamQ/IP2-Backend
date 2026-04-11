f19a69790397f5aafac9312b04b375476c982bc4
Barok-y <barokyeshibr@gmail.com>
2026-04-11 16:23:31 +0300

    typo

--- Ecomerece_Web_Backend/public/index.php
+++ Ecomerece_Web_Backend/public/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
 
-// import the databse config here abel, since u took database implementation!!
+// import the database config here abel, since u took database implementation!!
 
 require_once __DIR__ . '/../routes/api.php'
 
