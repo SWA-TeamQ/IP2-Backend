e7b966024b5dae9c1a2d96405e5c90fcbd48f186
Barok-y <barokyeshibr@gmail.com>
2026-04-21 21:10:40 +0300

    Full CLean up!

--- Ecomerece_Web_Backend/utils/array_helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!function_exists('app_pick')) {
-	// Small helper for safely reading array values.
-	function app_pick($data, $key, $fallback = null)
-	{
-		return is_array($data) && array_key_exists($key, $data) ? $data[$key] : $fallback;
-	}
-}
